eddede62721c467781ddfb6c15e2531aa2013383
mkaman <cengmkaman@gmail.com>
2021-04-13 01:12:13 +0300

    login controller integration tests  improvements

--- user/src/test/java/com/productheaven/user/controller/LoginControllerIntegrationTests.java
+++ user/src/test/java/com/productheaven/user/controller/LoginControllerIntegrationTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,9 +16,12 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestClientException;
 
+import com.productheaven.user.api.schema.request.UserCreateRequestDTO;
 import com.productheaven.user.api.schema.request.UserLoginRequestDTO;
 import com.productheaven.user.api.schema.response.BaseResponseDTO;
+import com.productheaven.user.api.schema.response.UserResponseDTO;
 import com.productheaven.user.persistence.entity.User;
 import com.productheaven.user.persistence.repository.UserRepository;
 import com.productheaven.user.util.TestUtils;
@@ -113,6 +117,29 @@ class LoginControllerIntegrationTests {
 		//then
 		assertEquals(HttpStatus.UNAUTHORIZED, createdEntity.getStatusCode());
 
+	}
+	
+	@Test
+	void usersShouldRegisterAndLoginSuccessfully () throws RestClientException, URISyntaxException {
+		
+		// given
+		UserCreateRequestDTO userRequestDTO = new UserCreateRequestDTO("sampleUsername","test@sampleemail.com","samplename","samplesurname","samplepass");
+		
+		//when
+		restTemplate.postForEntity(new URI("http://localhost:" + port + "/user"), userRequestDTO, UserResponseDTO.class);
+		
+		//given
+		UserLoginRequestDTO userLoginRequest = new UserLoginRequestDTO(userRequestDTO.getUsername(), userRequestDTO.getPassword()); 
+		
+		//when	
+		ResponseEntity<BaseResponseDTO> createdEntity = restTemplate.postForEntity(new URI("http://localhost:" + port + "/login"), userLoginRequest, BaseResponseDTO.class);
+		BaseResponseDTO response = createdEntity.getBody();
+		
+		//then
+		assertEquals(HttpStatus.OK, createdEntity.getStatusCode());
+		assertNull(response.getErrorMessage());
+
+		
 	}
 	
 }
